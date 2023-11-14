7bcd9e6bd1ca777ea7b9e4868646d332b8f3d5fc
Matheus Iam <matheusiam27@gmail.com>
2023-11-14 22:50:50 +0000

    Add dialog component and update button styles

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,7 +1,34 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 const InitialModal = () => {
-  return <div>WIP MODAL</div>;
+  return (
+    <Dialog open>
+      <DialogContent
+        className="bg-white text-black p-0 overflow-hidden"
+        suppressHydrationWarning
+      >
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl font-bold text-center">
+            Customize o seu servidor !!
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Dê uma personalidade ao seu servidor com um nome e uma foto. Você
+            pode alterar depois a qualquer momento.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default InitialModal;
